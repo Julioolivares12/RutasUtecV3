4b3445e00ca98c75c40e44715fd35b5df932594f
julio olivares <julioolivares90@gmail.com>
2017-04-16 16:43:48 -0600

    agregue el fragment de mapas y obtube la clave de google maps

--- app/src/main/java/mapas.java
+++ app/src/main/java/mapas.java
@@ -1,20 +1,39 @@
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.julio.rutasutecv3.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 /**
  * Created by julio on 04-15-17.
  */
 
-public class mapas  extends Fragment {
+public class mapas  extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         return inflater.inflate(R.layout.mapas,container,false);
     }
+    public void onMapReady(GoogleMap googleMap){
+
+        map = googleMap;
+        LatLng santiago = new LatLng(13.6462418,-89.10387830000002);
+        map.moveCamera(CameraUpdateFactory.newLatLng(santiago));
+    }
+
+
 }
