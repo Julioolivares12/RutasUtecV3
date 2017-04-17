0f221ecf6e393df9d10713df0d5c7e15a029cc40
Julio olivares <julioolivares90@gmail.com>
2017-04-17 16:05:54 -0600

    mejoras en la interfaz de usuario y agregado el mapa y otras modificaciones de permisos

--- app/src/main/java/com/example/julio/rutasutecv3/mapas_fragment.java
+++ app/src/main/java/com/example/julio/rutasutecv3/mapas_fragment.java
@@ -8,18 +8,53 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by julio on 4/17/17.
  */
 
-public class mapas_fragment extends Fragment {
+public class mapas_fragment extends Fragment implements OnMapReadyCallback {
     private static final String Tag="mapas_fragment";
-    private TextView txtmapas;
+    MapView mapView;
+    private GoogleMap googleMap;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.mapas_fragment,container,false);
-        txtmapas=(TextView)view.findViewById(R.id.txtmapas);
+        mapView=(MapView)view.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.onResume();
+        try
+        {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        }catch (Exception e){
+            //aqui va todo codigo
+            e.printStackTrace();
+        }
+        mapView.getMapAsync(this);
+      /*  mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                googleMap.setMyLocationEnabled(true);
+            }
+        });*/
+
         return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng latLng = new LatLng(13.6462418,-89.10387830000002);
+        float zoom=13;
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
+
     }
 }
