0b7ee9d187f7bb988381bef89aef9f48fac6e232
Julioolivares12 <julioolivares90@gmail.com>
2017-04-17 15:14:58 -0600

    agregue al fragment de inicio un ListView para mostrar las rutas mas utilizadas

--- app/src/main/java/com/example/julio/rutasutecv3/inicio_fragment.java
+++ app/src/main/java/com/example/julio/rutasutecv3/inicio_fragment.java
@@ -7,7 +7,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+
 
 /**
  * Created by julio on 4/17/17.
@@ -16,12 +20,18 @@ import android.widget.TextView;
 public class inicio_fragment extends Fragment {
     private static final String TAG = "inicio_fragment";
 
+    private ListView listInicio;
     private TextView txtinicio;
+
+    private String[] Rutas={"Ruta 7","Ruta 11","Ruta 29","Ruta 3","Ruta","Ruta 101"};
+    public ArrayAdapter<String>adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,Rutas);
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.inicio_fragment,container,false);
         txtinicio=(TextView)view.findViewById(R.id.txtinicio);
+        listInicio=(ListView)view.findViewById(R.id.ListInicio);
+        listInicio.setAdapter(adapter);
         return view;
     }
 }
